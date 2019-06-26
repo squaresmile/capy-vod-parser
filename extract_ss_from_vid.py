0bad2ff9fc095732fd76a9d702b9f11256ee2dc6
squaresmile <squaresmile@protonmail.com>
2019-06-25 22:02:43 -0400

    Remove dupe screenshots from output

--- extract_ss_from_vid.py
+++ extract_ss_from_vid.py
@@ -2,7 +2,6 @@ import os
 import argparse
 import multiprocessing
 import cv2
-import numpy as np
 from tqdm import tqdm
 import youtube_dl
 
@@ -11,17 +10,20 @@ YOUTUBE_DL_OPTIONS = {
     "format": "bestvideo[ext=mp4][height<=1080]/best[ext=mp4][height<=1080]",
     "outtmpl": "%(uploader)s-%(id)s.%(ext)s",
 }
-THRESHOLD = 0.2
 SKIP = 10
+TEMPLATE_MATCH_THRESHOLD = 0.2
+DUPE_THRESHOLD = 0.001
 DROP_TEXT_TEMPLATE = "template/drop_text.png"
 
 
 def recognize_drop_text(frame, template, name):
     res = cv2.matchTemplate(frame, template, cv2.TM_SQDIFF_NORMED)
-    loc = np.where(res < THRESHOLD)
+    # loc = np.where(res < THRESHOLD)
+    min_val, _, _, _ = cv2.minMaxLoc(res, None)
     # loc is empty if the frame doesn't match
     # return loc[0].size > 0
-    if loc[0].size > 0:
+    if min_val < TEMPLATE_MATCH_THRESHOLD:
+    # if loc[0].size > 0:
         cv2.imwrite(name, frame)
 
 
@@ -50,6 +52,21 @@ def extract_drop_screen(file, output_folder, processes):
     cap.release()
 
 
+def remove_dupe_images(output_folder):
+    files = os.listdir(output_folder)
+    files = sorted(files)
+    base_image = cv2.imread(os.path.join(output_folder, files[0]))
+    for file in files[1:]:
+        full_path = os.path.join(output_folder, file)
+        img = cv2.imread(full_path)
+        res = cv2.matchTemplate(base_image, img, cv2.TM_SQDIFF_NORMED)
+        min_val, _, _, _ = cv2.minMaxLoc(res, None)
+        if min_val < DUPE_THRESHOLD:
+            os.remove(full_path)
+        else:
+            base_image = img
+
+
 def run(link, quest, processes):
     quest_folder = os.path.join("input", str(quest))
     if not os.path.isdir(quest_folder):
@@ -65,6 +82,7 @@ def run(link, quest, processes):
         except (youtube_dl.utils.UnsupportedError, youtube_dl.utils.DownloadError):
             pass
     extract_drop_screen(file_name, quest_folder, processes)
+    remove_dupe_images(quest_folder)
 
 
 if __name__ == "__main__":
@@ -86,4 +104,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    run(args.input, args.quest, args.processes)
+    run(args.input, args.quest, args.num_processes)
